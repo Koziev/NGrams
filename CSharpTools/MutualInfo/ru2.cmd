4c2e58b20bdab5b222f6176439de377139ba8a82
inkoziev <mentalcomputing@gmail.com>
2018-02-28 11:56:17 +0400

    расширено описание

--- CSharpTools/MutualInfo/ru2.cmd
+++ CSharpTools/MutualInfo/ru2.cmd
@@ -1,1 +1,2 @@
-.\bin\x64\Release\MutualInfo.exe linkability2 -eols -o f:\tmp\mutual_info_2_ru.dat -maxngrams 20000000 -maxsentcount 1000000 -i f:\Corpus\Raw\ru\tokenized_w2v.txt
+.\bin\x64\Release\MutualInfo.exe linkability2 -eols -o f:\tmp\mutual_info_2_ru.dat -maxngrams 20000000 -maxsentcount 10000000 -i f:\Corpus\word2vector\ru\SENTx.corpus.w2v.txt
+
--- CSharpTools/MutualInfo/ru3.cmd
+++ CSharpTools/MutualInfo/ru3.cmd
@@ -1,1 +1,2 @@
-.\bin\x64\Release\MutualInfo.exe linkability3 -eols -o f:\tmp\mutual_info_3_ru.dat -maxngrams 20000000 -maxsentcount 1000000 -i f:\Corpus\Raw\ru\tokenized_w2v.txt
+.\bin\x64\Release\MutualInfo.exe linkability3 -eols -o f:\tmp\mutual_info_3_ru.dat -maxngrams 20000000 -maxsentcount 1000000 -i f:\Corpus\word2vector\ru\SENTx.corpus.w2v.txt
+
